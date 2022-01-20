1301f222b326cf9cce72cb1975a9d849b41ee29b
Qqkyu <kulmisiu007@gmail.com>
2022-01-20 22:11:09 +0100

    DFA ADT and language DFA definition

--- main.hs
+++ main.hs
@@ -1,7 +1,24 @@
 import System.IO
-import System.Environment
+
+data SetOfStates = SetOfStates [String]
+data SetOfInputSymbols = SetOfInputSymbols [String]
+data TransitionFunction = TransitionFunction [(String,String,String)]
+data StartState = StartState String
+data SetOfAcceptStates = SetOfAcceptStates [String]
+data DFA = DFA SetOfStates SetOfInputSymbols TransitionFunction StartState SetOfAcceptStates
+
+languageMinimalDFA :: DFA
+languageMinimalDFA = DFA (SetOfStates ["S"]) (SetOfInputSymbols ["a"]) (TransitionFunction [("S","a","S")]) (StartState "S") (SetOfAcceptStates [("S")])
+
+printAllowedRegexCharacters :: IO ()
+printAllowedRegexCharacters = do
+  putStrLn "Allowed constants:"
+  putStrLn "a - literal character"
+  putStrLn "Allowed operations:"
+  putStrLn "(R*) - Kleene star"
+  putStrLn "(RS) - concatenation"
+  putStrLn "(R|S) - alternation"
 
 main :: IO ()
 main = do
-  (regex : _) <- getArgs
-  print regex
+  printAllowedRegexCharacters
