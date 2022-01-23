d1eac6edd53580b2fe57d571cb627da39284a9ea
Qqkyu <kulmisiu007@gmail.com>
2022-01-23 20:54:04 +0100

    Space support, algorithm update

--- main.hs
+++ main.hs
@@ -2,16 +2,23 @@ import System.IO
 import AlgorithmStep1
 import AlgorithmStep2
 
+removeSpaces :: String -> String
+removeSpaces [] = []
+removeSpaces (a : as)
+  | a == ' '  = removeSpaces as
+  | otherwise = a : removeSpaces as
+
 printAllowedRegexCharacters :: IO ()
 printAllowedRegexCharacters = do
-  putStrLn "Allowed constants:"
+  putStrLn "---Allowed constants---"
   putStrLn "a - literal character"
   putStrLn "e - empty string (epsilon)"
-  putStrLn "Allowed operations:"
+  putStrLn "---Allowed operations---"
   putStrLn "R* - Kleene star"
   putStrLn "RS - concatenation"
   putStrLn "R+S - alternation"
-  putStrLn "Additional information:"
+  putStrLn "---Additional information---"
+  putStrLn "Spaces allowed"
   putStrLn "Unnested parentheses allowed"
   putStrLn "Redundant parentheses forbidden"
   putStrLn "Max one alternation in one part (a+a+a disallowed)"
@@ -23,4 +30,4 @@ main :: IO ()
 main = do
   printAllowedRegexCharacters
   r <- getLine
-  print (doesAlgorithmStep2Pass r)
+  print "End of program"
